6ce1899d53e8aa54280562e03dba80de23cd0966
Avi Jassra <avijassra@hotmail.com>
2026-09-26 09:45:35 -0400

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.py
@@ -0,0 +1,15 @@
+from itertools import pairwise
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x: x.start)
+
+        return all( b.start >= a.end for a,b in pairwise(intervals))
